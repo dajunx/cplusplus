357bf8922bfba00a7ff358c2897e1a712a40ec5f
dajunx <linjianjun_1990@hotmail.com>
2016-01-15 14:27:13 +0800

    优化算法,从1+2+...s.size() 变成 小于s.size()*98

--- without_repeat_substr.cpp
+++ without_repeat_substr.cpp
@@ -2,48 +2,61 @@
 #include <unordered_map>
 #include <string>
 using namespace std;
-/*Given a string, find the length of the longest substring without repeating characters. 
- *For example, the longest substring without repeating letters for "abcabcbb" is "abc", 
- *which the length is 3. For "bbbbb" the longest substring is "b", with the length of 1.
- *Subscribe to see which companies asked this question
- */
 int lengthOfLongestSubstring(string s)
 {
-	unordered_map<char,int> cache_substr;
-	int iSubStrLength = 0,lengthTemp = 0;
-	char* sbustr_temp = new char[s.length() + 1];
-	strcpy(sbustr_temp, s.c_str());
-	sbustr_temp[s.length()] = '\0';
-	char* p_substr = sbustr_temp;	// while循环一次，向后移动一个单词
-
-	while ( '\0' != *sbustr_temp)
+	int nSubstrMaxNonRepeatLength = 0;
+	unordered_map<char,vector<int> > cache_;
+	unordered_map<char,vector<int> >::iterator it;
+	vector<int> vec_char;
+	vector<int> vec_(1,0);
+	for (int i=32;i<127;i++)
 	{
-		if(cache_substr.find(*sbustr_temp) == cache_substr.end())
+		if(s.find((char)i) != string::npos)
 		{
-			cache_substr.insert(pair<char,int>(*sbustr_temp,1));
-			lengthTemp++;
+			vec_char.push_back(i);
+			cache_.insert(pair<char,vector<int> >((char)i,vec_));
 		}
-		else if(iSubStrLength <= lengthTemp) // 找到重复值 并且长度不小于缓存长度
-		{
-			iSubStrLength = lengthTemp;
-			cache_substr.clear();// 清理
-			lengthTemp = 0;
-			sbustr_temp = ++p_substr;
-			continue;
-		}
-		sbustr_temp ++;
 	}
-	if(iSubStrLength < lengthTemp) iSubStrLength = lengthTemp;
-	return iSubStrLength;
+
+	char* substr_temp = new char[s.length() + 1];
+	strcpy(substr_temp, s.c_str());
+	substr_temp[s.length()] = '\0';
+	char* p_substr = substr_temp;	// while循环一次，向后移动一个单词
+	while ( '\0' != *substr_temp)
+	{
+		for (vector<int>::iterator iIt = vec_char.begin(); iIt != vec_char.end();++iIt)
+		{
+			it = cache_.find((char)*iIt);
+			if ((char)*iIt == *substr_temp && it != cache_.end())
+			{
+				cache_[*iIt].push_back(0);
+			}
+			else if(it != cache_.end())
+			{
+				if(++it->second.back() > nSubstrMaxNonRepeatLength)
+					nSubstrMaxNonRepeatLength = it->second.back();
+			}
+		}
+		substr_temp ++;
+	}
+	if (s.size() > 0 && 0 == nSubstrMaxNonRepeatLength) nSubstrMaxNonRepeatLength++;
+	return nSubstrMaxNonRepeatLength;
 }
 int main(int argCount,char ** args)
 {
 	string input_str;
-	for (int i=0;i<361;i++)
+	for (int i=0;i<326;i++)
 	{
 		input_str.append("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ ");
 	}
 	input_str.append("abcdefghijklmnopqrstuvwxyzAB");
 	lengthOfLongestSubstring(input_str);
+	/*
+	for (int i=0;i<326;i++)
+	{
+		input_str.append("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ ");
+	}
+	input_str.append("abcdefghijklmnopqrstuvwxyzAB");
+	*/
 	return 0;
 };
