916cfa07399851ea9b1af0516321b6183489a6c5
林毅 <892888763@qq.com>
2017-06-03 17:20:34 +0800

    求解字符串数组最大前缀，算法2，完成，效率没有进一步提高

--- longest_common_prefix.cpp
+++ longest_common_prefix.cpp
@@ -16,7 +16,7 @@ string longestCommonPrefix(vector<string>& strs)
     for (;i < vec_size;++i)
     {
       //遍历到字符串数组中某个字符末尾
-      if (pos >= strs[i].length()) {
+      if (pos >= static_cast<int>(strs[i].length())) {
         break;
       }
 
@@ -34,28 +34,38 @@ string longestCommonPrefix(vector<string>& strs)
   return str_ret;
 }
 
+//总的算法：
+/*
+** 1、vector空？ 2、vector只有1个元素？ 
+** 3、对比逻辑：a、两两字符串找不到公共前缀 --> return 空串
+**            b、提取最大字符前缀 tmp，把tmp与vector<string>中下一个 进行对比
+**            c、类推
+*/
 string longestCommonPrefix2(vector<string>& strs)
 {
   //算法：每次去字符串数组中2个字符串最大前缀，然后拿这个前缀再去和下一个字符串对比，类推
-  string str_ret;
-  if (strs.size() == 0) { return str_ret;}
+  string equal_str;
+  if (strs.size() == 0) { return equal_str;}
 
   int strs_size = strs.size();
   int pos = 0, i = 1, mix_pos = 0;
-  string equal_str;
-  str_ret = strs[0];
+  equal_str = strs[0];
   while (i < strs_size)
   {
     pos = 0;
-    equal_str.clear();
-    mix_pos = str_ret.length() < strs[i].length() 
-              ? str_ret.length() : strs[i].length();
-    while(pos < mix_pos && str_ret.at(pos) == strs[i].at(pos)) {
-      equal_str.append(1, str_ret.at(pos));
+    mix_pos = equal_str.length() < strs[i].length() 
+              ? equal_str.length() : strs[i].length();
+    while(pos < mix_pos && equal_str.at(pos) == strs[i].at(pos)) {
       pos++;
     }
 
-    str_ret.swap(equal_str);
+    ///TODO string在swap函数中构造 linux端编译会提示如下error
+    /*
+    **error: cannot bind non-const lvalue reference of type 'std::__cxx11::basic_string<char>&' 
+    ** to an rvalue of type 'std::__cxx11::string {aka std::__cxx11::basic_string<char>}'
+    */
+    string tmp(equal_str, 0, pos);
+    equal_str.swap(tmp);
     //若两个字符串没有相同项，立马返回空前缀
     if (equal_str.empty()) {
       break;
@@ -63,15 +73,15 @@ string longestCommonPrefix2(vector<string>& strs)
     i++;
   }
 
-  return str_ret;
+  return equal_str;
 }
 
 int main()
 {
   vector<string> vec_str;
-  //vec_str.push_back("c");
-  vec_str.push_back("aa");
-  vec_str.push_back("ab");
+  vec_str.push_back("c");
+//   vec_str.push_back("acc");
+//   vec_str.push_back("ccc");
   longestCommonPrefix2(vec_str);
 
   return 0;
