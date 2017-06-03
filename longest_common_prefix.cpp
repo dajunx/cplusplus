72ef59117386e771675016690760c27c4c7dea6c
林毅 <892888763@qq.com>
2017-06-03 16:28:50 +0800

    求解字符串数组最大前缀，算法2(未完成)

--- longest_common_prefix.cpp
+++ longest_common_prefix.cpp
@@ -34,13 +34,45 @@ string longestCommonPrefix(vector<string>& strs)
   return str_ret;
 }
 
+string longestCommonPrefix2(vector<string>& strs)
+{
+  //算法：每次去字符串数组中2个字符串最大前缀，然后拿这个前缀再去和下一个字符串对比，类推
+  string str_ret;
+  if (strs.size() == 0) { return str_ret;}
+
+  int strs_size = strs.size();
+  int pos = 0, i = 1, mix_pos = 0;
+  string equal_str;
+  str_ret = strs[0];
+  while (i < strs_size)
+  {
+    pos = 0;
+    equal_str.clear();
+    mix_pos = str_ret.length() < strs[i].length() 
+              ? str_ret.length() : strs[i].length();
+    while(pos < mix_pos && str_ret.at(pos) == strs[i].at(pos)) {
+      equal_str.append(1, str_ret.at(pos));
+      pos++;
+    }
+
+    str_ret.swap(equal_str);
+    //若两个字符串没有相同项，立马返回空前缀
+    if (equal_str.empty()) {
+      break;
+    }
+    i++;
+  }
+
+  return str_ret;
+}
+
 int main()
 {
   vector<string> vec_str;
-//   vec_str.push_back("-ab");
-//   vec_str.push_back("abc");
-//   vec_str.push_back("abcd");
-  longestCommonPrefix(vec_str);
+  //vec_str.push_back("c");
+  vec_str.push_back("aa");
+  vec_str.push_back("ab");
+  longestCommonPrefix2(vec_str);
 
   return 0;
 }
