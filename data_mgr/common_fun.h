86cc517156131037025b061c3019e11766f25ecf
林毅 <892888763@qq.com>
2017-06-07 14:02:53 +0800

    修改解析命令函数 parse_cmd_and_content 的 bug；

--- data_mgr/common_fun.h
+++ data_mgr/common_fun.h
@@ -1,34 +1,30 @@
 #ifndef COMMON_FUN
 #define COMMON_FUN
 
-#include "data_mgr.h"
+//#include "data_mgr.h"
+#include "common_def.h"
 
 // 解析命令，返还一对命令/值 对;
 // set foo lin; get foo; shutdown;
 ///TODO 函数变成 非inline之后 链接错误，提示 main中与 network_mgr 中标识符冲突
-inline void parse_cmd_and_content(char* content, std::string& msg, std::string& key, std::string& value)
+inline void parse_cmd_and_content(char* content, std::vector<std::string>& vec_data)
 {
   if (!content) return;
+  if (vec_data.size() != 3) return;
 
   char* p_ch = content;
-  std::string pair;
-  bool first_para = true;
-
-  while (*p_ch != '\0') {
-    if (*p_ch != ' ') {
-      pair.push_back(*p_ch);
-    } else if (*p_ch == ' ') {
-      if (!pair.empty() ) {
-        first_para? msg = pair : key = pair;
-        first_para = false;
-      }
-      pair.clear();
+  int data_pos = 0;
+  std::string single_data;
+  while (p_ch != NULL) {
+    if (*p_ch != ' ' && *p_ch != '\0') {
+      single_data.push_back(*p_ch);
+    } else if (*p_ch == ' ' || *p_ch == '\0') {
+      vec_data[data_pos++] = single_data;
+      single_data.clear();
     }
-    p_ch++;
-  }
 
-  if ( !pair.empty() ) {
-    first_para? msg = pair : value = pair;
+    if (*p_ch == '\0') break;
+    p_ch++;
   }
 }
 
