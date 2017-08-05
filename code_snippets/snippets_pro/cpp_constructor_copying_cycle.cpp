a366f5c3a73c09e728905b35996a8a696978b21b
lin <892888763@qq.com>
2017-08-05 16:30:43 +0800

    c++ 赋值运算符 返回参数不使用引用 导致的=> copying 与 constructor 形成循环

---
+++ code_snippets/snippets_pro/cpp_constructor_copying_cycle.cpp
@@ -0,0 +1,31 @@
+//c++ 赋值操作符 和 拷贝构造 产生的问题
+
+#include <iostream>
+#include <map>
+#include <vector>
+#include <string>
+
+struct test1
+{
+  int x;
+  test1(int w=0) {x= w;}
+  
+  //如下若值返回，则会在copying 和 构造函数中循环
+  //const test1 operator = (const test1& o)
+
+  const test1& operator = (const test1& o)
+  {
+    x = o.x;
+    return *this;
+  }
+  test1(const test1& o)
+  {
+    *this = o;
+  }
+};
+int main()
+{
+  test1 tt1(5);
+  test1 tt2 = tt1;
+  return 0;
+}
