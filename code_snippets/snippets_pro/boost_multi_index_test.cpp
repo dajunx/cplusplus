452bd9adcf7862f6b0a51f631462cd782d1554bc
林毅 <892888763@qq.com>
2017-08-23 18:35:09 +0800

    新增加 boost multi_index 使用例子，涉及遍历、更新和插入动作

--- code_snippets/snippets_pro/boost_multi_index_test.cpp
+++ code_snippets/snippets_pro/boost_multi_index_test.cpp
@@ -3,6 +3,7 @@
 #include <boost/multi_index_container.hpp>
 #include <boost/multi_index/member.hpp>
 #include <boost/multi_index/ordered_index.hpp>
+#include <boost/foreach.hpp>
 #include <algorithm>
 #include <iostream>
 #include <iterator>
