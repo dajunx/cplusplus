beb8c1aa581a628e80dc631c3c3f58441279bcdc
林毅 <892888763@qq.com>
2017-09-06 18:49:47 +0800

    boost 协程测试

---
+++ code_snippets/snippets_pro/coroutine_test.cpp
@@ -0,0 +1,31 @@
+//boost 协程例子
+/*
+结果:
+foo1 1
+foo2 101
+foo2 3
+*/
+#include <iostream>
+#include <boost/asio/yield.hpp>
+#include <boost/asio/coroutine.hpp>
+
+boost::asio::coroutine c;
+
+void foo(int i)
+{
+  reenter(c)
+  {
+    yield std::cout<<"foo1 "<<i<<std::endl;
+    fork foo(100);
+    yield std::cout<<"foo2 "<< i+1<<std::endl;
+  }
+}
+
+int main()
+{
+  foo(1);
+  foo(2);
+  foo(3);
+
+  return 0;
+}
