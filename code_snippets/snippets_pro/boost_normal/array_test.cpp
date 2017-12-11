9d3912e1debf2c0fe1adc84ed7c0c23350aaf236
林毅 <892888763@qq.com>
2017-12-11 18:48:33 +0800

    代码仓库增加常规例子

---
+++ code_snippets/snippets_pro/boost_normal/array_test.cpp
@@ -0,0 +1,23 @@
+/*
+  兼容STL的容器，是固定大小的数组的包装
+*/
+#include <iostream>
+#include <boost/array.hpp>
+#include <string>
+#include <algorithm>
+int main()
+{
+  typedef boost::array<std::string, 4> array;
+  array a;
+  a[0] = "lin";
+  a.at(1) = "jian";
+  *a.rbegin() = "jun";
+  array b = {"lin", "jian", "jun" };
+  std::sort(a.begin(), a.end());
+  for (array::const_iterator it = a.begin(); it != a.end(); ++it) {
+    std::cout<< *it <<std::endl;
+  }
+  std::cout<<a.size()<<std::endl;
+  std::cout<<a.max_size()<<std::endl;
+  return 0;
+}
