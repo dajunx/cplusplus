fe5cb12e5feb7055fdcc294bc1418c32d0f91eeb
林毅 <892888763@qq.com>
2017-12-12 15:13:59 +0800

    整理go 基础用法代码路径，增加go使用mysql 小测试代码(连接的是本地mysql)

---
+++ go_test/connect_mysql.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	// Á¬½Ómysql
+	db, err := sql.Open("mysql", "root:@/test")
+	if err != nil {
+		fmt.Println("connect mysql err.")
+	}
+
+	rows, err := db.Query("select * from test1")
+	if err != nil {
+		fmt.Println("Query select err.")
+	}
+
+	for rows.Next() {
+		var id int
+		var name string
+		rows.Scan(&id, &name)
+		fmt.Println(id)
+		fmt.Println(name)
+	}
+}
