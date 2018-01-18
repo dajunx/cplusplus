45c5224298072a6efcf7db70afa7d93aaf89b4da
林毅 <892888763@qq.com>
2018-01-18 13:32:09 +0800

    优化，增加错误处理逻辑

--- go_test/getMemdbIpInfo.go
+++ go_test/getMemdbIpInfo.go
@@ -7,7 +7,7 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
-func getIpAndIndex(doc *goquery.Document, nodeInfo string) {
+func getIpAndDirIndex(doc *goquery.Document, nodeInfo string) {
 	doc.Find(nodeInfo).Each(func(i int, s *goquery.Selection) {
 		tdAttr, exists := s.Attr("ondblclick")
 		if exists && strings.Contains(tdAttr, "Copy") {
@@ -30,11 +30,12 @@ func getIpAndIndex(doc *goquery.Document, nodeInfo string) {
 	})
 }
 
-func getAllSidNum(Url *string) []string {
+func getAllSidNum(Url *string) ([]string, error) {
 	var ret []string
 	doc, err := goquery.NewDocument(*Url)
 	if err != nil {
-		fmt.Print("err")
+		fmt.Print(err)
+		return ret, err
 	}
 
 	doc.Find("body a").Each(func(i int, s *goquery.Selection) {
@@ -43,22 +44,26 @@ func getAllSidNum(Url *string) []string {
 			ret = append(ret, s.Text())
 		}
 	})
-	return ret
+	return ret, err
 }
 
 func main() {
 	var Url string = "http://www.xxxxx.com" //target url
-	sids := getAllSidNum(&Url)
+	sids, errSids := getAllSidNum(&Url)
+	if errSids != nil {
+		fmt.Print(errSids)
+		return
+	}
 	fmt.Println(sids)
 	//遍历所有频道
 	for _, v := range sids {
 		url := Url + "?sid=" + v //拼凑单个语音组网页
 		doc, err := goquery.NewDocument(url)
 		if err != nil {
-			fmt.Print("err")
+			fmt.Print(err)
 		}
 		//fmt.Printf("sid=%s\n", v) //sid号
-		getIpAndIndex(doc, ".listtable .oum1 td")
-		getIpAndIndex(doc, ".listtable .oum2 td")
+		getIpAndDirIndex(doc, ".listtable .oum1 td")
+		getIpAndDirIndex(doc, ".listtable .oum2 td")
 	}
 }
