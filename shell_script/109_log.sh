73e1b85010e9fa4dadc5cb7f1fb209670243e737
林毅 <892888763@qq.com>
2018-02-27 09:27:59 +0800

    日志处理脚本 移动数据源时增加校验

--- shell_script/109_log.sh
+++ shell_script/109_log.sh
@@ -1,27 +1,30 @@
 #获取download下来的109组日志
 cd 'C:\Users\Administrator\Desktop\109_log'
-cp `find 'C:\Users\Administrator\Downloads' -name "xx.xx.xx.xx*"` .
+findTextReocordRes=$(find 'C:\Users\Administrator\Downloads' -name "*text_reocord*")
+if [ $findTextReocordRes ]; then
+    mv $findTextReocordRes .;
+    echo "move text reocord";
+fi
 
 #获取所有日志日期 格式 yyyy-mm-dd
 logDays=$(ls -l | awk -F" " '{print $9}' | awk -F"reocord_" '{print $2}' | awk -F"." '{print $1}')
 
-#创建目录并拷贝对应日志压缩包进去
 for day in ${logDays[@]}; do
-    #生成日期目录并拷贝对应压缩包、解压
+    #把每天的日志压缩包解压到各自日期目录中
     mkdir $day;
-    dailyLog=$(find . -name "*$day*.tar.gz");
-    cp $dailyLog $day;
+    tar xf `echo "*$day*.tar.gz"` -C $day;
+        #第二种实现方式
+        #dailyLog=$(find . -name "*$day*.tar.gz");
+        #tar xf ${dailyLog:2:58} -C $day;
     cd $day;
-    tar xf $dailyLog;
-    rm $dailyLog;
 
-    #汇总文本为每天两个文件 hourLogs每日原始日志文件集合
-    hourLogs=$(find . -name "xx.xx.xx.xx*");
+    #汇总文本为每天两个文件
+    hourLogs=$(find . -name "*text_reocord*");
     cat `ls | grep "${hourLogs:2:45}0[0-9]"` > "${hourLogs:2:45}00~11";
     cat `ls | grep "${hourLogs:2:45}1[0-1]"` >> "${hourLogs:2:45}00~11";
     cat `ls | grep "${hourLogs:2:45}1[2-9]"` > "${hourLogs:2:45}12~23";
     cat `ls | grep "${hourLogs:2:45}2[0-3]"` >> "${hourLogs:2:45}12~23";
-    rm $hourLogs; #删除源日志文件
+#rm $hourLogs; #删除源日志文件
 
     #校验
     #TODo
