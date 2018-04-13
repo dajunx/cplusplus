0c4173be8aac3545b45e17b925e5dd9a9dada8f3
林毅 <892888763@qq.com>
2018-04-13 20:02:57 +0800

    利用测试数据的一行一行输入漏洞，精简代码

--- nowcoder/reverse_words.cpp
+++ nowcoder/reverse_words.cpp
@@ -12,24 +12,14 @@
 
 int main()
 {
-    std::string str_src, str_tmp;
+    std::string target_str, str_tmp;
     while(std::cin >> str_tmp)
     {
-        str_src.append(str_tmp + " ");
+        target_str.insert(0, " ");
+        target_str.insert(0, str_tmp);
     }
-    str_src.pop_back();
-    
-    std::string target_string;
-    std::string::size_type space_pos;
-    while ((space_pos = str_src.find_first_of(" ", 0)) != std::string::npos)
-    {
-        target_string.insert(0, str_src.substr(0, space_pos));
-        target_string.insert(0, " ");
-        str_src.erase(0, space_pos+1);
-    }
-
-    target_string.insert(0, str_src);
-    std::cout<<target_string<<std::endl;
+    target_str.pop_back();
+    std::cout<<target_str<<std::endl;
     
     return 0;
 }
