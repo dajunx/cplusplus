f60cf34edac9d0f1d8249843de44294fb22e572a
林毅 <892888763@qq.com>
2018-06-11 17:16:49 +0800

    常规更新

--- code_snippets/snippets_pro/boost_normal/calculate_fun_duration_time.cpp
+++ code_snippets/snippets_pro/boost_normal/calculate_fun_duration_time.cpp
@@ -1,13 +1,12 @@
 ﻿//程序计算 时间差
+//转换ptime 为string类型：boost::posix_time::to_simple_string
 #include <boost/date_time/posix_time/posix_time.hpp>
 #include <iostream>
 #include <map>
 #include <sstream>
 #include <string>
 
-int main() {
-  boost::posix_time::ptime start =
-      boost::posix_time::microsec_clock::local_time();
+void cost_time_fun() {
   std::map<std::string, int> mm;
   std::stringstream map_key;
   for (int i = 1000; i < 100000; ++i) {
@@ -15,23 +14,22 @@ int main() {
     map_key << i;
     mm.insert(std::pair<std::string, int>(map_key.str(), i));
   }
-  boost::posix_time::ptime end =
+}
+
+int main() {
+  boost::posix_time::ptime start_time =
+      boost::posix_time::microsec_clock::local_time();
+  cost_time_fun();
+ 
+  boost::posix_time::ptime end_time =
       boost::posix_time::microsec_clock::local_time();
 
-  boost::posix_time::time_duration cost = end - start;
-  std::cout << cost.total_milliseconds() << "ms" << std::endl;
+  boost::posix_time::time_duration time_costs = end_time - start_time;
 
-  start = boost::posix_time::microsec_clock::local_time();
-  std::map<int, int> mm1;
-  for (int i = 1000; i < 100000; ++i) {
-    map_key.str("");
-    map_key << i;
-    map_key.str();
-    mm1.insert(std::pair<int, int>(i, i));
-  }
-  end = boost::posix_time::microsec_clock::local_time();
-
-  cost = end - start;
-  std::cout << cost.total_milliseconds() << "ms" << std::endl;
+  std::cout<<"start time:"　<< boost::posix_time::to_simple_string(start_time) << std::endl;
+    //output: start time:2018-Jun-11 17:01:09.910169
+  std::cout<<"end time:"　<< boost::posix_time::to_simple_string(end_time) << std::endl;
+    //output: end time:2018-Jun-11 17:01:25.580065
+  std::cout << "cost_time_fun 运算耗时:" << time_costs.total_milliseconds() << "ms" << std::endl;
   return 0;
 }
