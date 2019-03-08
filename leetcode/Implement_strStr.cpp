e87386f4311b0b8b73c5fca25c32c1e82dd0ab2e
lin_work_win <892888763@qq.com>
2019-03-08 15:07:26 +0800

    perf(test.cases)：测试用例输出颜色化，修改了用例不通过流程逻辑

--- leetcode/Implement_strStr.cpp
+++ leetcode/Implement_strStr.cpp
@@ -16,6 +16,7 @@
 #include <string>
 #include <sstream>
 #include <assert.h>
+#include <windows.h> // 用来使用 绘制颜色
 using namespace std;
 
 class Solution {
@@ -69,20 +70,25 @@ public:
 // 运行算法 + 输出结果
 void impl_test_fun(Solution & slu, std::string const& haystack, std::string const& needle, bool expect_res) {
   bool ret = slu.strStr(haystack, needle) != -1 ? true : false;
-  if (ret != expect_res) {
-    assert(false);
-  } else {
-    std::stringstream sstr_haystack, sstr_needle, sstr_expect_res;
-    sstr_haystack << "\"" << (haystack.empty()?"":haystack) << "\"";
-    sstr_needle << "\"" << (needle.empty()?"":needle) << "\"";
-    sstr_expect_res << (expect_res?"true":"false");
+  int ret_color_num = ret == expect_res? 10:12;
+  HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
-    std::cout<<"[test] compre<"<<sstr_haystack.str()<<", "<<sstr_needle.str()<<">, is "<<sstr_expect_res.str()<<" success."<<std::endl;
-  }
+  std::stringstream sstr_haystack, sstr_needle, sstr_expect_res, sstr_ret;
+  sstr_haystack << "\"" << (haystack.empty()?"":haystack) << "\"";
+  sstr_needle << "\"" << (needle.empty()?"":needle) << "\"";
+  sstr_expect_res << (expect_res?"true":"false");
+  sstr_ret << (ret == expect_res?"true":"false");
+
+  std::cout<<"[test] compre<"<<sstr_haystack.str()<<", "<<sstr_needle.str()<<">, is "<<sstr_expect_res.str();
+
+  // 高亮校验结果颜色，通过 青色表示， 不通过 红色表示
+  SetConsoleTextAttribute(hConsole, ret_color_num); // 对success 高亮
+  std::cout<<" ["<<sstr_ret.str()<<"]."<<std::endl;
+  SetConsoleTextAttribute(hConsole, 7);  // 恢复常规颜色
 }
 
 // 自定义测试用例，用于测试算法
-void self_test_case() {
+void self_test_cases() {
   Solution slu;
   /** 测试用例
    * imple_test_fun 参数解释:
@@ -90,17 +96,25 @@ void self_test_case() {
    *   2.第三个参数为 子字符串 needle
    *   3.第四个参数为 期望的算法结果
    */
-  impl_test_fun(slu, "mississippi", "issip", true);
+  impl_test_fun(slu, "mississippi", "issip", true); // 子字符串要对比两次才能找到
   impl_test_fun(slu, "missi", "ss", true);
+  impl_test_fun(slu, "missi", "ssa", false);
+  impl_test_fun(slu, "a", "a", true); // 完全匹配
+  impl_test_fun(slu, "missi", "mi", true); // 首段匹配
+  impl_test_fun(slu, "missi", "si", true); // 尾端段匹配
+  impl_test_fun(slu, "mi", "missi", false); // 父字符串不够的case
 
   // 三种特殊 case
   impl_test_fun(slu, "", "ss", false);
   impl_test_fun(slu, "missi", "", true);
   impl_test_fun(slu, "", "", true);
+
+  // 错误例子
+  //impl_test_fun(slu, "missi", "si", false);
 }
 
 int main() {
-  self_test_case();
+  self_test_cases();
 
   return 0;
 }
