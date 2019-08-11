8b085f8ae798f1e3a2f4e7b31aae9bb18e5996c5
kaka <892888763@qq.com>
2019-08-11 14:33:19 +0800

    add(MFC_DLL.test):增加dll试验的测试工程

---
+++ MFC_DLL_test/test_dll/test_dll/test_dllDlg.h
@@ -0,0 +1,32 @@
+
+// test_dllDlg.h : header file
+//
+
+#pragma once
+
+
+// Ctest_dllDlg dialog
+class Ctest_dllDlg : public CDialogEx
+{
+// Construction
+public:
+	Ctest_dllDlg(CWnd* pParent = NULL);	// standard constructor
+
+// Dialog Data
+	enum { IDD = IDD_TEST_DLL_DIALOG };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
