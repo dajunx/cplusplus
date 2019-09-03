2708970026eefad5a3117aa034b84cbe6ac832de
lin_shengli_work <892888763>
2019-09-03 16:05:12 +0800

    update(MFC.multi_dialog_communicate):增加父子对话框 相互交互(发送消息)例子，向控件发送消息例子

---
+++ MFC/multi_dialog_communicate/multi_dialog_communicate/message.h
@@ -0,0 +1,6 @@
+
+#pragma once
+
+#define WM_USER_DEFINE_MSG	(WM_USER+0x02001) // 用户自定义消息
+#define WM_CHILD_USER_DEFINE_MSG	(WM_USER+0x02002) // 子对话框 用户自定义消息
+#define WM_FATHER_2_CHILD_MSG	(WM_USER+0x02003) // 父窗口向子窗口发送消息
